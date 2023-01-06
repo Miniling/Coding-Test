4fe7e5625dcd6864aca8cc5edd5ff4dec8b51273
Miniling <bsm3925@naver.com>
2023-01-06 23:13:07 +0900

    DCT.c

---
+++ 소프티어/DCT.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int DCT[8];
+	int sort = 0;
+
+	for(int i=0; i<8; i++) {
+		scanf("%d", &DCT[i]);
+	}
+
+	for(int i=0; i<7; i++) {
+		if(DCT[i] == DCT[i+1]-1) {
+			sort = 1;
+		}
+		else if(DCT[i] == DCT[i+1]+1) {
+			sort = -1;
+		} else {
+			sort = 0;
+			printf("mixed");
+			break;
+		}
+	}
+
+	if(sort==1)	printf("ascending");
+	else if(sort==-1)	printf("descending");
+
+
+	return 0;
+}
