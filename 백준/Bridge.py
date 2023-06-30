039a2758f730c1e300fb079f9877c9832caa82af
Miniling <bsm3925@naver.com>
2023-06-30 22:15:36 +0900

    Bridge.py

--- 백준/Bridge.py
+++ 백준/Bridge.py
@@ -7,6 +7,8 @@ T = int(input())
 for case in range(T):
     n, m = map(int, input().split(' '))
     answer = 0
+
+    # 수동 계산, 시간 초과!
     # east = [0]*m
 
     # for bridge in itertools.combinations(east, n):
